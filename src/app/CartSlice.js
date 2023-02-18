b4ad17bb416e89038f94e8192b43d9763e9bef4e
Annguyen <an.nguyentruong.andrew@gmail.com>
2023-02-19 00:24:44 +0700

    update

---
+++ src/app/CartSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+
+const initialState ={
+    cartState:false,
+}
+
+const CartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    setOpenCart:(state,action)=>{
+        state.cartState = action.payload.cartState
+
+    },
+    setCloseCart:(state,action)=>{
+        state.cartState = action.payload.cartState
+
+    }
+  }
+});
+
+export const {setOpenCart,setCloseCart} = CartSlice.actions
+
+export const selectCartState = (state) => state.cart.cartState
+
+
+export default CartSlice.reducer
+
+
